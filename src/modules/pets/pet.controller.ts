db5f0d29f182e6a1b495220ff7a8779827034d62
harshtarang7 <tarangharsh7@gmail.com>
2024-08-28 14:53:02 +0530

    updated get pet info api with generic response

--- src/modules/pets/pet.controller.ts
+++ src/modules/pets/pet.controller.ts
@@ -1,17 +1,25 @@
 import { Controller, Get } from '@nestjs/common';
 import { PetService } from './pet.service';
 import { PetInfo } from 'src/models/pet.info.entity';
+import { SuccessResponse } from 'src/common/success.response';
+import { ErrorResponse } from 'src/common/error.response';
 
 @Controller('pets')
 export class PetController {
   constructor(private readonly petService: PetService) {}
 
   @Get('/get-all-pets')
-  async getAllPets(): Promise<PetInfo[]> {
+  async getAllPets(): Promise<SuccessResponse<PetInfo[]> | ErrorResponse> {
     try {
-      return this.petService.getAllPets();
+      const petInfo = await this.petService.getAllPets();
+
+      return new SuccessResponse(petInfo);
     } catch (error) {
-      throw error;
+      return new ErrorResponse(
+        'Error occured while fetching the pet info',
+        true,
+        error,
+      );
     }
   }
 }
